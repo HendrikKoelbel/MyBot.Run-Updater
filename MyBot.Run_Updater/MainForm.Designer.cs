4f6d07f748f370c229dc2d70f38ddc80d2251e65
Hendrik Kölbel <Henni9599@icloud.com>
2019-04-24 08:05:34 +0200

    Projektdateien hinzufügen.

---
+++ MyBot.Run_Updater/MainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace MyBot.Run_Updater
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_checkUpdate = new System.Windows.Forms.Button();
+            this.progressBarMain = new System.Windows.Forms.ProgressBar();
+            this.labelstatus = new System.Windows.Forms.Label();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btn_checkUpdate
+            // 
+            this.btn_checkUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_checkUpdate.Font = new System.Drawing.Font("Arial", 11.25F);
+            this.btn_checkUpdate.Location = new System.Drawing.Point(12, 303);
+            this.btn_checkUpdate.Name = "btn_checkUpdate";
+            this.btn_checkUpdate.Size = new System.Drawing.Size(136, 26);
+            this.btn_checkUpdate.TabIndex = 0;
+            this.btn_checkUpdate.Text = "Check for Update";
+            this.btn_checkUpdate.UseVisualStyleBackColor = true;
+            this.btn_checkUpdate.Click += new System.EventHandler(this.Btn_checkUpdate_Click);
+            // 
+            // progressBarMain
+            // 
+            this.progressBarMain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBarMain.Location = new System.Drawing.Point(12, 348);
+            this.progressBarMain.Name = "progressBarMain";
+            this.progressBarMain.Size = new System.Drawing.Size(425, 23);
+            this.progressBarMain.TabIndex = 1;
+            // 
+            // labelstatus
+            // 
+            this.labelstatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelstatus.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelstatus.Location = new System.Drawing.Point(0, 374);
+            this.labelstatus.Name = "labelstatus";
+            this.labelstatus.Size = new System.Drawing.Size(449, 48);
+            this.labelstatus.TabIndex = 2;
+            this.labelstatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_cancel.Font = new System.Drawing.Font("Arial", 11.25F);
+            this.btn_cancel.Location = new System.Drawing.Point(301, 303);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(136, 26);
+            this.btn_cancel.TabIndex = 0;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.Btn_cancel_Click);
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxLog.Location = new System.Drawing.Point(13, 13);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.ReadOnly = true;
+            this.richTextBoxLog.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedHorizontal;
+            this.richTextBoxLog.Size = new System.Drawing.Size(424, 270);
+            this.richTextBoxLog.TabIndex = 3;
+            this.richTextBoxLog.Text = "";
+            this.richTextBoxLog.TextChanged += new System.EventHandler(this.richTextBoxLog_TextChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 422);
+            this.Controls.Add(this.richTextBoxLog);
+            this.Controls.Add(this.labelstatus);
+            this.Controls.Add(this.progressBarMain);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_checkUpdate);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(465, 530);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(325, 390);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyBot Updater by Hendrik Kölbel";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_checkUpdate;
+        private System.Windows.Forms.ProgressBar progressBarMain;
+        private System.Windows.Forms.Label labelstatus;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
+    }
+}
+
---
+++ MyBot.Run_Updater/MainForm.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Microsoft.Win32;
+
+namespace MyBot.Run_Updater
+{
+    public partial class MainForm : Form
+    {
+
+        public RegistryKey MyBot_set = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\MyBot_Updater");
+        public RegistryKey MyBot_get = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\MyBot_Updater");
+        public string myBotNewVersionURL = "https://raw.githubusercontent.com/MyBotRun/MyBot/master/LastVersion.txt";
+        public string myBotDownloadURL = null;
+        public string userDownloadFolder = personalFolders.GetPath(KnownFolder.Downloads);
+        public string newMyBotVersion = null;
+        public string currentMyBotVersion = null;
+        public string currentMyBotFileName = null;
+        public string currentMyBotPath = null;
+        public Thread downloadThread;
+        public Thread unzipThread;
+        public MainForm()
+        {
+            InitializeComponent();
+            Logger("Hey, I'm Hendrik. Your MyBot Updater!", Color.Blue);
+        }
+
+        public void Btn_checkUpdate_Click(object sender, EventArgs e)
+        {
+            Logger("Open editor!", Color.Black);
+            Logger("Please go in your MyBot folder and select the Application .exe (MyBot.run)", Color.Black);
+            OpenFileDialog openCurrentMyBot = new OpenFileDialog();
+            openCurrentMyBot.Title = "Choose MyBot.run.exe";
+            openCurrentMyBot.Filter = "Application file|*.exe";
+            openCurrentMyBot.InitialDirectory = userDownloadFolder;
+            if (openCurrentMyBot.ShowDialog() == DialogResult.OK)
+            {
+                Logger("You chose the application = " + openCurrentMyBot.FileName, Color.Black);
+
+                MyBot_set.SetValue("mybot_path", Path.GetDirectoryName(openCurrentMyBot.FileName));
+                Logger("MyBot path = " + MyBot_set.GetValue("mybot_path").ToString(), Color.Red, 3);
+
+                MyBot_set.SetValue("mybot_exe", Path.GetFullPath(openCurrentMyBot.FileName));
+                Logger("MyBot xxe path = " + MyBot_set.GetValue("mybot_exe").ToString(), Color.Red, 3);
+
+                string latestMyBotPath = Path.GetFullPath(openCurrentMyBot.FileName);
+                var latestMyBotVersionInfo = FileVersionInfo.GetVersionInfo(latestMyBotPath);
+                currentMyBotVersion = "v" + latestMyBotVersionInfo.FileVersion;
+
+                MyBot_set.SetValue("mybot_version", currentMyBotVersion);
+                Logger("MyBot current version = " + MyBot_set.GetValue("mybot_version"), Color.Red, 3);
+
+                WebClient myBotNewVersionClient = new WebClient();
+                Stream stream = myBotNewVersionClient.OpenRead(myBotNewVersionURL);
+                StreamReader reader = new StreamReader(stream);
+                String content = reader.ReadToEnd();
+
+                var sb = new StringBuilder(content.Length);
+                foreach (char i in content)
+                {
+                    if (i == '\n')
+                    {
+                        sb.Append(Environment.NewLine);
+                    }
+                    else if (i != '\r' && i != '\t')
+                        sb.Append(i);
+                }
+
+                content = sb.ToString();
+
+                var vals = content.Split(
+                                            new[] { Environment.NewLine },
+                                            StringSplitOptions.None
+                                        )
+                            .SkipWhile(line => !line.StartsWith("[general]"))
+                            .Skip(1)
+                            .Take(1)
+                            .Select(line => new
+                            {
+                                Key = line.Substring(0, line.IndexOf('=')),
+                                Value = line.Substring(line.IndexOf('=') + 1).Replace("\"", "").Replace(" ", "")
+
+                            });
+                /*
+                 * Complete output: vals.FirstOrDefault().Key  + vals.FirstOrDefault().Value;
+                 * To get the first key + value thy this code line
+                 * Only the first value: vals.FirstOrDefault().Value;
+                 **/
+                newMyBotVersion = vals.FirstOrDefault().Value;
+                MyBot_set.SetValue("mybot_version", newMyBotVersion);
+                Logger("MyBot new version = " + MyBot_set.GetValue("mybot_version").ToString(), Color.Red);
+                myBotDownloadURL = "https://github.com/MyBotRun/MyBot/releases/download/MBR_" + newMyBotVersion + "/MyBot-MBR_" + newMyBotVersion + ".zip";
+                if (currentMyBotVersion != newMyBotVersion)
+                {
+                    Logger("Wait for download...", Color.Black);
+                    DialogResult wantUpdateYoN = MessageBox.Show("New version " + newMyBotVersion + " is available. Would you like to install it?", "New version", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (wantUpdateYoN == DialogResult.Yes)
+                    {
+                        Logger("The bot will be updated.", Color.Blue);
+                        if (File.Exists(userDownloadFolder + "/MyBot-MBR_" + newMyBotVersion + ".zip"))
+                        {
+                            Logger("Zip is allready exists!", Color.Black);
+                            UnZip_MyBot();
+                        }
+                        else
+                        {
+                            Logger("Start Download...", Color.Blue);
+                            startDownload();
+                        }
+                    }
+                    else if (wantUpdateYoN == DialogResult.No)
+                    {
+                        Logger("Canceled...", Color.Red);
+                    }
+                }
+                else
+                {
+                    Logger("Your version is the newest!", Color.Green);
+                }
+            }
+            else
+            {
+                Logger("No file selected!", Color.Red);
+            }
+        }
+
+        #region MainForm Load
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //getRegistryValue();
+        }
+        #endregion
+
+        #region Unzip
+        public void UnZip_MyBot()
+        {
+            if (!File.Exists(userDownloadFolder + "/MyBot-MBR_" + newMyBotVersion))
+            {
+                BackgroundWorker unzipBackgroundWorker = new BackgroundWorker();
+                unzipThread = new Thread(() =>
+                {
+                    // To report progress from the background worker we need to set this property
+                    unzipBackgroundWorker.WorkerReportsProgress = true;
+                    unzipBackgroundWorker.ProgressChanged += new ProgressChangedEventHandler(unzipBackgroundWorker_ProgressChanged);
+                    // This event will be raised on the worker thread when the worker starts
+                    unzipBackgroundWorker.DoWork += new DoWorkEventHandler(unzipBackgroundWorker_DoWork);
+                    // This event will be raised when we call ReportProgress
+                    unzipBackgroundWorker.RunWorkerAsync();
+                });
+                unzipThread.Start();
+                //using (var unzip = new unzip(userDownloadFolder + "/MyBot-MBR_" + newMyBotVersion + ".zip"))
+                //{
+
+                //    Logger("Unzip the MyBot-MBR_" + newMyBotVersion + ".zip", Color.Black);
+                //    unzip.ExtractToDirectory(userDownloadFolder);
+                //}
+                unzipBackgroundWorker.ProgressChanged += new ProgressChangedEventHandler(unzipBackgroundWorker_ProgressChanged);
+                Logger("Unzip the MyBot-MBR_" + newMyBotVersion + ".zip", Color.Black);
+            }
+            else
+            {
+                Logger("Folder allready exists!", Color.Blue);
+            }
+        }
+        void unzipBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Your background task goes here           
+            ZipFile.ExtractToDirectory(userDownloadFolder + "/MyBot-MBR_" + newMyBotVersion + ".zip", userDownloadFolder);
+        }
+        // Back on the 'UI' thread so we can update the progress bar
+        void unzipBackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // The progress percentage is a property of e
+            progressBarMain.Value = e.ProgressPercentage;
+        }
+        #endregion
+
+        #region Start Download and Progress bar
+        public void startDownload()
+        {
+            downloadThread = new Thread(() =>
+            {
+                WebClient myBotDownloadNewVersion = new WebClient();
+                myBotDownloadNewVersion.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                myBotDownloadNewVersion.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                myBotDownloadNewVersion.DownloadFileAsync(new Uri(myBotDownloadURL), userDownloadFolder + "/MyBot-MBR_" + newMyBotVersion + ".zip");
+            });
+            downloadThread.Start();
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                double bytesIn = double.Parse(e.BytesReceived.ToString());
+                double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+                double percentage = bytesIn / totalBytes * 100;
+                Logger("Downloading " + ConvertBytesToMegabytes(bytesIn) + " MB/s of " + ConvertBytesToMegabytes(totalBytes) + " MB/s", Color.Blue, 2);
+                progressBarMain.Value = int.Parse(Math.Truncate(percentage).ToString());
+            });
+        }
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                Logger("Fully Downloaded", Color.Green);
+                progressBarMain.Value = 0;
+                UnZip_MyBot();
+            });
+        }
+        #endregion
+
+        #region Scroll Auto to Bottom
+        // bind this method to its TextChanged event handler:
+        // richTextBox.TextChanged += richTextBox_TextChanged;
+        private void richTextBoxLog_TextChanged(object sender, EventArgs e)
+        {
+            // set the current caret position to the end
+            richTextBoxLog.SelectionStart = richTextBoxLog.Text.Length;
+            // scroll it automatically
+            richTextBoxLog.ScrollToCaret();
+        }
+        #endregion
+
+        #region Logger Method
+        /* public void Logger(RichTextBox TextEventLog, Color TextColor, string EventText)
+         * For custom RichTextBox
+         **/
+        public void Logger(string EventText, Color TextColor, int Option = 1)
+        {
+            RichTextBox TextEventLog = richTextBoxLog;
+            Label status = labelstatus;
+            if (Option == 1)
+            {
+                if (TextEventLog.InvokeRequired)
+                {
+                    TextEventLog.BeginInvoke(new Action(delegate
+                    {
+                        Logger(EventText, TextColor, Option);
+                    }));
+                    return;
+                }
+                string nDateTime = DateTime.Now.ToString("hh:mm:ss tt") + ": ";
+
+                // color text.
+                TextEventLog.SelectionStart = TextEventLog.Text.Length;
+                TextEventLog.SelectionColor = TextColor;
+
+                labelstatus.Text = EventText;
+
+                // newline if first line, append if else.
+                if (TextEventLog.Lines.Length == 0)
+                {
+                    TextEventLog.AppendText(nDateTime + EventText);
+                    TextEventLog.ScrollToCaret();
+                    TextEventLog.AppendText(Environment.NewLine);
+                }
+                else
+                {
+                    TextEventLog.AppendText(nDateTime + EventText + Environment.NewLine);
+                    TextEventLog.ScrollToCaret();
+                }
+            }
+            else if (Option == 2)
+            {
+                labelstatus.Text = EventText;
+            }
+            else if (Option == 3)
+            {
+                // Nothing
+            }
+        }
+        #endregion
+
+        #region Registry Methods
+        public void getRegistryValue()
+        {
+            if (MyBot_get != null)
+            {
+                currentMyBotVersion = (string)MyBot_get.GetValue("mybot_version", null);
+                currentMyBotFileName = (string)MyBot_get.GetValue("mybot_exe", null);
+                currentMyBotPath = (string)MyBot_get.GetValue("mybot_path", null);
+            }
+            else
+            {
+                MyBot_set.SetValue("mybot_path", currentMyBotPath);
+                MyBot_set.SetValue("mybot_exe", currentMyBotFileName);
+                MyBot_set.SetValue("mybot_version", currentMyBotVersion);
+            }
+        }
+        #endregion
+
+        #region Convert
+        public static string ConvertBytesToMegabytes(double bytes)
+        {
+            double result = (bytes / 1024f) / 1024f;
+            return result.ToString("0.00");
+        }
+
+        public static string ConvertKilobytesToMegabytes(double kilobytes)
+        {
+            double result = kilobytes / 1024f;
+            return result.ToString("0.00");
+        }
+        #endregion
+
+        private void Btn_cancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
